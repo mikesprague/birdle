f75c799f81ff8606e9d3e8d024148d24e284a4ca
Michael Sprague <ms388@cornell.edu>
2022-02-04 10:29:16 -0500

    :construction: Add WIP

--- src/app.js
+++ src/app.js
@@ -1,4 +1,43 @@
-import { setData } from './lib/local-storage';
+import { setData, getData, clearData } from './lib/local-storage';
+import { keys } from './lib/keys';
 
-setData('birdle-app', 'birdle!');
-console.log('birdle!');
+const appContainer = document.querySelector('.app');
+const titleContainer = document.querySelector('.title-container');
+const guessesContainer = document.querySelector('.guesses-container');
+const keyboardContainer = document.querySelector('.keyboard-container');
+
+const guessesRows = [
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+];
+
+guessesRows.forEach((guessRow, guessRowIndex) => {
+  const rowEl = document.createElement('div');
+  rowEl.setAttribute('id', `guessRow-${guessRowIndex}`);
+  rowEl.classList.add('guess-row');
+  guessRow.forEach((guess, guessIndex) => {
+    const guessEl = document.createElement('div');
+    guessEl.setAttribute('id', `guessRow-${guessRowIndex}-guess-${guessIndex}`);
+    guessEl.classList.add('guess');
+    rowEl.append(guessEl);
+  });
+  guessesContainer.append(rowEl);
+  console.log(rowEl);
+});
+
+const handleKey = (event) => {
+  console.log(event.target.id);
+};
+
+keys.forEach((keyVal, keyIndex) => {
+  const buttonEl = document.createElement('button');
+  buttonEl.textContent = keyVal;
+  buttonEl.classList.add('key');
+  buttonEl.setAttribute('id', keyVal);
+  buttonEl.addEventListener('click', handleKey);
+  keyboardContainer.append(buttonEl);
+});
