2f1e58735a035dfe04a339fce45f040b4894c85a
Michael Sprague <mikesprague@gmail.com>
2022-02-04 23:41:39 -0500

    :art: Improve keyboard

--- src/lib/keys.js
+++ src/lib/keys.js
@@ -1,12 +1,18 @@
 module.exports.initKeys = (keys, keyHandler) => {
   const keyboardContainer = document.querySelector('.keyboard-container');
   keys.forEach((keyVal) => {
-    const buttonEl = document.createElement('button');
-    buttonEl.textContent = keyVal;
-    buttonEl.classList.add('key');
-    buttonEl.setAttribute('id', keyVal);
-    buttonEl.addEventListener('click', keyHandler, true);
-    keyboardContainer.append(buttonEl);
+    if (keyVal === '--') {
+      const breakEl = document.createElement('div');
+      breakEl.classList.add('break-flex');
+      keyboardContainer.append(breakEl);
+    } else {
+      const buttonEl = document.createElement('button');
+      buttonEl.textContent = keyVal;
+      buttonEl.classList.add('key');
+      buttonEl.setAttribute('id', keyVal);
+      buttonEl.addEventListener('click', keyHandler, true);
+      keyboardContainer.append(buttonEl);
+    }
   });
 };
 
@@ -21,6 +27,7 @@ module.exports.keys = [
   'I',
   'O',
   'P',
+  '--',
   'A',
   'S',
   'D',
@@ -30,6 +37,7 @@ module.exports.keys = [
   'J',
   'K',
   'L',
+  '--',
   'ENTER',
   'Z',
   'X',
