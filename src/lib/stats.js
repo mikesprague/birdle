f56c5cee23313f4891a843761563522ca4436303
Michael Sprague <mikesprague@gmail.com>
2022-02-08 12:39:30 +0000

    :alembic: Experimenting

--- src/lib/stats.js
+++ src/lib/stats.js
@@ -55,9 +55,9 @@ module.exports.updateStats = (won = true) => {
 };
 
 module.exports.createShareText = () => {
-  const absentEmoji = '⚫'; // '🦤';
-  const presentEmoji = '🟡'; // '🐤';
-  const correctEmoji = '🟢'; // '🦜';
+  const absentEmoji = '🦉'; // '⚫'; // '🦉';
+  const presentEmoji = '🐥'; // '🟡'; // '🐥';
+  const correctEmoji = '🦚'; // '🟢'; // '🦚';
   const gameState = getData('gameState');
   const gameId = gameState.gameId;
   const finalRow = gameState.currentRow >= 5 ? 6 : gameState.currentRow + 1;
