d8fb35d495a45807b5bc6108e8fbb169aa6e9079
Michael Sprague <mikesprague@gmail.com>
2022-03-14 14:42:28 +0000

    :alembic: Experimenting

--- api/get-wordle-solution.js
+++ api/get-wordle-solution.js
@@ -1,26 +1,47 @@
+const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer');
 
 module.exports = async (req, res) => {
-  try {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
+  const browser = await puppeteer.launch(
+    process.env.AWS_EXECUTION_ENV
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {
+          args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        },
+  );
+  const page = await browser.newPage();
+  await page.goto('https://www.nytimes.com/games/wordle/index.html');
 
-    const page = await browser.newPage();
-    await page.goto('https://www.nytimes.com/games/wordle/index.html');
+  const gameState = await page.evaluate(() =>
+    // eslint-disable-next-line no-undef
+    window.localStorage.getItem('nyt-wordle-state'),
+  );
+  const { solution } = JSON.parse(gameState);
+  console.log(JSON.parse(gameState));
+  console.log(solution);
 
-    const gameState = await page.evaluate(() =>
-      // eslint-disable-next-line no-undef
-      window.localStorage.getItem('nyt-wordle-state'),
-    );
-    const { solution } = JSON.parse(gameState);
-    console.log(JSON.parse(gameState));
-    console.log(solution);
+  await browser.close();
 
-    await browser.close();
+  const now = new Date();
+  const midnight = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + 1,
+    0,
+    0,
+    0,
+  );
+  const secondsUntilMidnight = Math.round(
+    (midnight.getTime() - now.getTime()) / 1000,
+  );
 
-    res.status(200).json(solution);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+  // res.setHeader(
+  //   'Cache-Control',
+  //   `max-age=${secondsUntilMidnight}, s-maxage=${secondsUntilMidnight}`,
+  // );
+  res.status(200).json(solution);
 };
