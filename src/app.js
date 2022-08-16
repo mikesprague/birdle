d5fac7998c1af9012884665281abfd0f59cba32c
Michael Sprague <mikesprague@gmail.com>
2022-08-16 01:20:18 +0000

    :truck: Revert js file name

--- src/app.js
+++ src/app.js
@@ -2,19 +2,29 @@ import {
   getBirdleOfDay,
   initAnalytics,
   initGame,
-  // initServiceWorker,
+  initServiceWorker,
+  isDev,
 } from './lib/helpers';
 import { getData } from './lib/local-storage';
 
 import './styles.scss';
 
 (async () => {
+  if (isDev()) {
+    const { title } = window.document;
+
+    window.document.title = `DEV ${title}`;
+  }
+
   const birdle = getBirdleOfDay();
   const firstVisit =
     (getData('gameState') === null || getData('gameState') === undefined) &&
     (getData('stats') === null || getData('stats') === undefined);
 
-  // initServiceWorker(firstVisit);
+  initServiceWorker(firstVisit);
   await initGame(birdle.day, firstVisit);
-  initAnalytics();
+
+  if (!isDev()) {
+    initAnalytics();
+  }
 })();
